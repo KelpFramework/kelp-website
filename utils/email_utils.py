791b38ff27efc7e9512fea87c9a7ee07c7e73de5
pxav <pxav.studios@gmail.com>
2021-04-13 19:28:03 +0200

    Add documentation to email_utils.py

--- utils/email_utils.py
+++ utils/email_utils.py
@@ -1,3 +1,10 @@
+"""
+This file handles email traffic caused by the kelp website,
+which is used to verify user accounts or reset user passwords.
+
+The email credentials can be set up in the configuration file.
+"""
+
 import smtplib
 import threading
 from email.mime.multipart import MIMEMultipart
@@ -8,6 +15,15 @@ from __init__ import render_template, application_name
 
 
 def send_email(receiver, content: MIMEMultipart, subject):
+    """
+    Sends an email to the given recipient with the credentials
+    and name set up in the app config. The application name will be
+    used as sender display name.
+
+    :param receiver:    The email address of the recipient.
+    :param content:     The content of the email address to send.
+    :param subject:     The subject of the email.
+    """
     def send():
         content["Subject"] = f"{application_name} {subject}"
         content["From"] = f"{application_name} <{configuration.mailing_system_email_address}>"
@@ -30,10 +46,21 @@ def send_email(receiver, content: MIMEMultipart, subject):
                 content.as_string()
             )
 
+    # send the mail via async thread to avoid UI inconveniences
     threading.Thread(target=send, daemon=True).start()
 
 
 def send_verification_email(receiver, verification_code):
+    """
+    Sends the account verification email to a given email address.
+    This function automatically constructs the html content for the page,
+    so you only have to pass the verification code, which will then be inserted
+    accordingly.
+
+    :param receiver:            The email address of the user you want to send the
+                                email to. A user name/uuid is not valid here.
+    :param verification_code:   The verification code to send to the user.
+    """
     email_content = MIMEMultipart("alternative")
     email_content.attach(MIMEText(
         render_template("email_templates/verification_email.html", code=verification_code),
@@ -44,6 +71,13 @@ def send_verification_email(receiver, verification_code):
 
 
 def send_user_password_reset_email(receiver, code):
+    """
+    Sends the password reset email to the given recipient.
+
+    :param receiver:    The email address of the user you want to send the email to.
+                        A user name or uuid is not valid here.
+    :param code:        The password reset code to apply to display in the email.
+    """
     email_content = MIMEMultipart("alternative")
     email_content.attach(MIMEText(
         render_template("email_templates/password_reset_email.html", code=code),
